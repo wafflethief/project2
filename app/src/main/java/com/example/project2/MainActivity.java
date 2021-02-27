0a56938486b6c8d4e3930bf0dd9c89a44f498930
wafflethief <janlouie810@gmail.com>
2021-02-26 20:42:35 -0500

    New screen functionality

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button launchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // when click button, call launch()
+        // attach OnClickListener to the button
+        launchButton = (Button) findViewById(R.id.button1);
+        launchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                beginActivity();
+            }
+        });
+    }
+
+    private void beginActivity(){
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/project2/MainActivity2.java
+++ app/src/main/java/com/example/project2/MainActivity2.java
@@ -3,12 +3,24 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        backButton = (Button) findViewById(R.id.button2);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
